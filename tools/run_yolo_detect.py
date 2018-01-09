cb2edd307bf0e3b995786abbb65d536a9a509773
Roberto Munoz <rmunoz@uc.cl>
2018-01-09 01:14:57 -0300

    Added data folder. It contains example images and alphabet

--- tools/run_yolo_detect.py
+++ tools/run_yolo_detect.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 input_dir="/mnt/backup/NVR/futbol_mexico/yolo"
 output_dir="/mnt/backup/VA/futbol_mexico/yolo/images"
+n_sample=30
 
 yolo_test_file="cfg/futbol_mexico/test.txt"
 yolo_data_file="cfg/futbol_mexico/yolo_metric.data"
@@ -15,14 +16,14 @@ data=pd.read_csv(yolo_test_file, header=None)
 data.columns=['file_in']
 data['file_out']=data['file_in'].str.replace(input_dir, output_dir)
 
-data_sample=data.sample(n=10).reset_index(drop=True)
+data_sample=data.sample(n=n_sample).reset_index(drop=True)
 
 time_start = datetime.now()
 for i in range(len(data_sample)):
     
     command=[ "./darknet", "detector", "test",
         yolo_data_file, yolo_config_file, yolo_weight_file,
-        data_sample.loc[i,"file_in"], "-thresh", yolo_thresh,
+        data_sample.loc[i,"file_in"], "-thresh", "{:.2f}".format(yolo_thresh),
         "-out", data_sample.loc[i,"file_out"] ]
 
     print(command)
