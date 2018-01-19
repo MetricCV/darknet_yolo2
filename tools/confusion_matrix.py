9ca7a9905d762f768ea4927ba0fc2b2672972e63
sarpon <sebarpon@gmail.com>
2018-01-19 09:41:53 -0300

    finished function confusion_detection_ann_images in confusion_matrix file

--- tools/confusion_matrix.py
+++ tools/confusion_matrix.py
@@ -48,7 +48,7 @@ relationdict (dict) is the mapping between the first column in ann [values(relat
 
 Output:
 --------
-
+act_pred (numpy.array) matrix with where first axis represent the actual class and second axis represent the detected class
 '''
     inv_relationdict=dict(zip(list(relationdict.values()),list(relation_dict.keys())))
     detec=json.load(open(detect_path,"r"))
@@ -56,38 +56,26 @@ Output:
     act_pred=np.zeros([len(relationdict.keys())+1,len(relationdict.keys())+1])
     pann=[]
     dann=[]
-    for i in ann:
-        pann.append(box(float(i["left"]),float(i["top"]),float(i["right"]),float(i["bottom"])))
     if len(detect)>0:    
-        IOU=np.zeros([len(ann),len(detect)])
-        for i in detect:
-            dann.append(box(float(i["left"]),float(i["top"]),float(i["right"]),float(i["bottom"])))
-        for i in range(0,len(pann)):
-            for j in range(0,len(dann)):
-                IOU[i,j]=(pann[i].intersection(dann[j]))/(pann[i].union(dann[j]))
-        argMAXIOU=np.argmax(IOU)
-        MAXIOU=np.amax(IOU)
-        for i in range(0,len(pann)):
-            if MAXIOU[i]>=Treshold:
-                num_class_ann=relationdict[ann[i]["class"]]
-                num_class_detec=relationdict[detec[argMAXIOU[i]]["class"]]
-                act_pred[num_class_ann,num_class_detec]+=1
-            #what happend when is less than the threshold?    
+	    for i in ann:
+	        pann.append(box(float(i["left"]),float(i["top"]),float(i["right"]),float(i["bottom"])))
+	        IOU=np.zeros([len(ann),len(detect)])
+	        for j in detect:
+	            dann.append(box(float(j["left"]),float(j["top"]),float(j["right"]),float(j["bottom"])))
+	        for j in range(0,len(pann)):
+	            for k in range(0,len(dann)):
+	                IOU[j,k]=(pann[j].intersection(dann[k]))/(pann[j].union(dann[k]))
+	        argMAXIOU=np.argmax(IOU)
+	        MAXIOU=np.amax(IOU)
+	        for j in range(0,len(pann)):
+	            num_class_ann=relationdict[ann[j]["class"]]
+	            num_class_detec=relationdict[detec[argMAXIOU[j]]["class"]]
+	            if MAXIOU[j]>=Treshold:
+	                act_pred[num_class_ann,num_class_detec]+=1# we go to the detected class
+	            else
+	            	act_pred[num_class_ann,len(relationdict.keys())]+=1# we go to the last entri which means that we didn't classify anything        
     else:
-        #what happend when we do not detect anything
-        return()
-    # if len(detec)>0:
-    #     for i in range(0,len(detec)):
-    #         dictfromann=[item for item in ann item["class"]==detec[i]["class"]]
-    #         if len(dictfromann)>0:   
-    #             b1=box(float(detec[i]["left"]),float(detec[i]["top"]),float(detec[i]["right"]),float(detec[i]["bottom"]))
-    #             MAXIOU=0
-    #             for j in range(0,dictfromann):
-    #                 b2=box(float(dictfromann[j]["left"]),float(dictfromann[j]["top"]),float(dictfromann[j]["right"]),float(dictfromann[j]["bottom"]))
-    #                 b1Ub2_area=b1.union(b2).area
-    #                 b1interb2_area=b1.intersection(b2).area
-    #                 MAXIOU=max(MAXIOU,(b1interb2_area/b1Ub2_area))
-    #             if MAXIOU>=Treshold:
-    #                 true_pos+=1
-    #             else:
-                         
+    	for i in ann:    #what happend when we do not detect anything
+    		num_class_ann=relationdict[i["class"]]
+    		act_pred[num_class_ann,len(relationdict.keys())]+=1	
+    return(act_pred)
